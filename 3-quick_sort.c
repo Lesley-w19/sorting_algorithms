6ab1d24e7136e88ae3076b11615801a69e94843b
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-17 17:57:24 +0300

    chnages to the if quickSort function

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,39 +1,4 @@
 #include "sort.h"
-/**
- * quick_sort - a fucntion that sorts a list usin the Lomunto
- * quick sort algorithm
- *
- * @array: the array to be sorted
- * @size: size of the array to be sorted
- * 
- * Return: a sorted list
- */
-void quick_sort(int *array, size_t size)
-{
-    if (!array || size < 2)
-        return;
-    
-    quickSort(array, 0, size - 1, size);
-}
-  
-/**
- * quicksort - function that sorts an array of integers
- * in ascending order using the Quick sort algorithm
- * @array: pointer to the array to sort
- * @low: start of the array
- * @high: end of the array
- * @size: size of the array
- * Return: nothing void
- */
-void quickSort(int arr[], int low, int high, size_t size)
-{
-    if (low < high) {
-        int index = partition(arr, low, high, size);
-  
-        quickSort(arr, low, index - 1, size);
-        quickSort(arr, index + 1, high, size);
-    }
-}
 
 /**
  * swap - function that swap value
@@ -43,9 +8,11 @@ void quickSort(int arr[], int low, int high, size_t size)
  */
 void swap(int* a, int* b)
 {
-    int t = *a;
+    int temp;
+
+    temp = *a;
     *a = *b;
-    *b = t;
+    *b = temp;
 }
 
 /**
@@ -57,21 +24,57 @@ void swap(int* a, int* b)
  * @size: size of the array
  * Return: returns the nwes index oh the value
  */
-int partition(int arr[], int low, int high, size_t size)
+int partition(int *array, int low, int high, size_t size)
 {
-    int i, j;
-    int pivot = arr[high]; 
-    i = (low - 1); 
-        
+    int pivot = array[high]; 
+    int i = (low - 1); 
+    int j;       
   
-    for (j = low; j <= high - 1; j++)
-    {
-        if (arr[j] < pivot) {
-            i++; 
-            swap(&arr[i], &arr[j]);
+   for (j = low; j <= high - 1; j++) {
+       
+        if (array[j] < pivot) {
+            i++;
+            swap(&array[i], &array[j]);
+            print_array(array, size);
         }
     }
-    swap(&arr[i + 1], &arr[high]);
-    print_array(arr, size);
-    return (i);
+    swap(&array[i + 1], &array[high]);
+    return (i + 1);
 }
+/**
+ * quickSort - function that sorts an array of integers
+ * in ascending order using the Quick sort algorithm
+ * @array: pointer to the array to sort
+ * @low: start of the array
+ * @high: end of the array
+ * @size: size of the array
+ * Return: nothing void
+ */
+void quickSort(int *array, int low, int high, size_t size)
+{
+    int index;
+    if (low < high) {
+        index = partition(array, low, high, size);
+  
+        quickSort(array, low, index - 1, size);
+        quickSort(array, index + 1, high, size);
+    }
+}
+
+/**
+ * quick_sort - a fucntion that sorts a list usin the Lomunto
+ * quick sort algorithm
+ *
+ * @array: the array to be sorted
+ * @size: size of the array to be sorted
+ * 
+ * Return: a sorted list
+ */
+void quick_sort(int *array, size_t size)
+{
+    if (array == NULL || size < 2)
+        return;
+    
+    quickSort(array, 0, size - 1, size);
+}
+
