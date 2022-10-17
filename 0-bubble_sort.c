be2deba806069580e87c26df7e73553ea9e1f2bd
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-17 12:37:12 +0300

    betty styling

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,11 +1,11 @@
 #include "sort.h"
 /**
  * bubble_sort - a function that sorts an array of integers
- * in ascending order using the Bubble sort algorithm 
+ * in ascending order using the Bubble sort algorithm
  *
  * @array: the array of integers
  * @size: size of the array
- * 
+ *
  * Return: nothing
  */
 
@@ -14,18 +14,17 @@ void bubble_sort(int *array, size_t size)
     size_t i, j;
     int temp;
 
-    if (!array)
+    if (!array){
         return;
+    }
 
-    if (size == 0 || size == 1)
+    if (size == 0 || size == 1){
         return;
+    }
 
-    for (i = 0; i < size - 1; i++)
-    {
-        for (j = 0; j < size - i - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
+    for (i = 0; i < size - 1; i++){
+        for (j = 0; j < size - i - 1; j++){
+            if (array[j] > array[j + 1]){
                 temp = array[j];
                 array[j] = array[j + 1];
                 array[j + 1] = temp;
