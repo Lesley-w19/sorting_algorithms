2fad4164fd75c9529fc1b4860099ae916c7cf2fd
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-11-04 22:41:25 +0300

    betty coding style-- -------------- removed all indentation

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,32 +8,29 @@
  * Return: the sorted list
  */
 void insertion_sort_list(listint_t **list)
+{listint_t *afterVal;
+if (list == NULL || *list == NULL)
+return;
+
+afterVal = (*list)->next;
+
+while (afterVal != NULL){
+while (afterVal->prev && afterVal->prev->n > afterVal->n)
 {
-        listint_t *afterVal;
-
-        if (list == NULL || *list == NULL)
-            return;
-        
-        afterVal = (*list)->next;
-
-        while (afterVal != NULL)
-        {
-            while (afterVal->prev && afterVal->prev->n > afterVal->n)
-            {
-                afterVal->prev->next = afterVal->next;
-                if (afterVal->next)
-                        afterVal->next->prev = afterVal->prev;
-                    afterVal->next = afterVal->prev;
-                    afterVal->prev = afterVal->next->prev;
-                    afterVal->next->prev = afterVal;
+afterVal->prev->next = afterVal->next;
+if (afterVal->next)
+afterVal->next->prev = afterVal->prev;
+afterVal->next = afterVal->prev;
+afterVal->prev = afterVal->next->prev;
+afterVal->next->prev = afterVal;
                 
-                if (afterVal->prev == NULL)
-                        *list = afterVal;
-                else
-                        afterVal->prev->next = afterVal;
+if (afterVal->prev == NULL)
+*list = afterVal;
+else
+afterVal->prev->next = afterVal;
                 
-                print_list(*list);
-        }
-            afterVal = afterVal->next;
-        }    
+print_list(*list);
+}
+afterVal = afterVal->next;
+}    
 }
