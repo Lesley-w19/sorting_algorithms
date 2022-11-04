c1db4a5b41b87fbdc82b3866874e596c224a284a
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-11-04 22:42:44 +0300

    betty-code styling ------------ removed all triling indentations

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,29 +10,29 @@
  */
 void selection_sort(int *pmArray, size_t size)
 {
-        size_t i, j, min;
-        int temp, *array = pmArray;
+size_t i, j, min;
+int temp, *array = pmArray;
 
-        if (!pmArray || size < 2)
-            return;
+if (!pmArray || size < 2)
+return;
 
-        for (i = 0; i < size - 1; i++)
-        {
-            min = i;
+for (i = 0; i < size - 1; i++)
+{
+min = i;
 
-            for (j = i + 1; j < size; j++)
-            {
-                if (array[j] < array[min])
+for (j = i + 1; j < size; j++)
+{
+if (array[j] < array[min])
                     min = j;
                 
-                if (min != i)
-                {
-                    temp = array[min];
-                    array[min] = array[i];
-                    array[i] = temp;
-                    print_array(array, size);
-                }
+if (min != i)
+{
+temp = array[min];
+array[min] = array[i];
+array[i] = temp;
+print_array(array, size);
+}
                    
-            }
-        } 
+}
+} 
 }
