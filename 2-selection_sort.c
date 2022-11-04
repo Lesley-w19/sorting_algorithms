d1f6e48579790d0cb5413a119b20c97dfc8db43a
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-11-04 23:03:11 +0300

    renamed the variable parameter in the function

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -3,17 +3,17 @@
  * selection_sort - a function that sorts an array of integers
  * in ascending order using the Selection sort algorithm
  *
- * @array: the array
+ * @pmArray: the array
  * @size: size of the array
  *
  * Return: list sorted by selection sort ascending order
  */
-void selection_sort(int *array, size_t size)
+void selection_sort(int *pmArray, size_t size)
 {
 size_t i, j, min;
-int temp, *array = array;
+int temp, *array = pmArray;
 
-if (!array || size < 2)
+if (!pmArray || size < 2)
 return;
 
 for (i = 0; i < size - 1; i++)
