962a6569fad199f0854c6f99623dd49e0282c2ae
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-11-04 22:57:30 +0300

    betty code styling validation --------- removed white trailing saces

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,20 +1,23 @@
 #include "sort.h"
 /**
- * insertion_sort_list - a function that sorts a doubly linked list 
- * of integers in ascending order 
+ * insertion_sort_list - a function that
+ * sorts a doubly linked list
+ * of integers in ascending order
  *
  * @list: the list to sort by insertion sort
- * 
+ *
  * Return: the sorted list
  */
 void insertion_sort_list(listint_t **list)
-{listint_t *afterVal;
+{
+listint_t *afterVal;
 if (list == NULL || *list == NULL)
 return;
 
 afterVal = (*list)->next;
 
-while (afterVal != NULL){
+while (afterVal != NULL)
+{
 while (afterVal->prev && afterVal->prev->n > afterVal->n)
 {
 afterVal->prev->next = afterVal->next;
@@ -23,14 +26,14 @@ afterVal->next->prev = afterVal->prev;
 afterVal->next = afterVal->prev;
 afterVal->prev = afterVal->next->prev;
 afterVal->next->prev = afterVal;
-                
+
 if (afterVal->prev == NULL)
 *list = afterVal;
 else
 afterVal->prev->next = afterVal;
-                
+
 print_list(*list);
 }
 afterVal = afterVal->next;
-}    
+}
 }
